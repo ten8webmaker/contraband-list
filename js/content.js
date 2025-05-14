e068bcc11107faedadcdc9978aa6dd34420ca6f5
ten8webmaker <judegmr1@gmail.com>
2025-05-14 16:53:11 -0700

    Update content.js

--- js/content.js
+++ js/content.js
@@ -604,7 +604,7 @@ export async function fetchStaff() {
         // add link to editor if there isn't one
         for (const editor of staff) {
             if (!editor.link)
-                editor.link = "https://1dot0list.pages.dev/#/leaderboard/user/" +
+                editor.link = "https://drug-list.pages.dev/#/leaderboard/user/" +
                     editor.name.toLowerCase().replaceAll(" ", "_");
         }
         return staff;
